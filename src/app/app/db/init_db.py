c31cd9c22d5561f508339c2d0b1a920899a62e0f
Ayman <asuleihi@gmail.com>
2025-01-27 23:19:02 +0000

    Initial commit

---
+++ src/app/app/db/init_db.py
@@ -0,0 +1,26 @@
+from sqlmodel import Session, select
+
+from app.crud.crud_user import user as crud_user
+from app.core.config import settings
+from app.models.user import User, UserCreate  # noqa: F401
+
+# make sure all SQLModel models are imported (app.models) before initializing DB
+# otherwise, SQLModel might fail to initialize relationships properly
+# for more details: https://github.com/tiangolo/full-stack-fastapi-postgresql/issues/28
+
+
+def init_db(session: Session) -> None:
+    # Tables should be created with Alembic migrations
+    # But if you don't want to use migrations, create
+    # the tables un-commenting the next line
+    # Base.metadata.create_all(bind=engine)
+    user = session.exec(
+        select(User).where(User.email == settings.FIRST_SUPERUSER)
+    ).first()
+    if not user:
+        user_in = UserCreate(
+            email=settings.FIRST_SUPERUSER,
+            password=settings.FIRST_SUPERUSER_PASSWORD,
+            is_superuser=True,
+        )
+        user = crud_user.create(db=session, obj_in=user_in)
---
+++ src/app/app/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, Depends
+from fastapi.routing import APIRoute
+from fastapi.staticfiles import StaticFiles
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+from sqlmodel import Session
+
+from app.api.api_v1.api import api_router
+from app.core.config import settings
+from app.db.engine import engine
+
+
+def custom_generate_unique_id(route: APIRoute):
+    return f"{route.tags[0]}-{route.name}"
+
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        # allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["POST", "PUT", "DELETE"],
+        allow_headers=["*"],
+    )
+]
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    docs_url=f"{settings.API_V1_STR}/docs",
+    generate_unique_id_function=custom_generate_unique_id,
+    middleware=middleware,
+)
+
+@app.get("/api/v1/", tags=["read_root"])
+def read_root():
+    return {"Hello": "World"}
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
