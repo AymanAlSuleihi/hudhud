487c7a1bcf608cb2f19ca008f9fe529c2fa9401f
Ayman <asuleihi@gmail.com>
2025-02-23 16:09:00 +0000

    Update wordparser to capture tail and handle spaces + cleanup

--- src/backend/app/app/services/word/word_parser.py
+++ src/backend/app/app/services/word/word_parser.py
@@ -11,65 +11,64 @@ class WordParser:
         self.session = session
         self.epigraph = epigraph
 
+    def _process_text(
+        self,
+        text: str,
+        classification: str = None,
+        attributes: dict = {},
+        previous_word: Word = None,
+    ) -> Word:
+        if not text or text.isspace():
+            return None
+
+        for word_text in text.split():
+            db_word = crud_word.get_by_word(self.session, word=word_text)
+            if not db_word or db_word.classification != classification:
+                db_word = crud_word.create(
+                    db=self.session,
+                    obj_in=WordCreate(
+                        word=word_text,
+                        classification=classification,
+                        attributes=attributes,
+                    ),
+                )
+
+            db_word = crud_word.link_to_epigraph(
+                self.session,
+                word=db_word,
+                epigraph_id=self.epigraph.id,
+            )
+            if previous_word:
+                db_word = crud_word.link_words(
+                    self.session,
+                    from_word=db_word,
+                    to_word=previous_word,
+                )
+
+        return db_word
+
     def parse(self):
         """Parse epigraph text xml and create words."""
         root = ET.fromstring(self.epigraph.epigraph_text)
         words = []
 
         for element in root.iter():
-            word_text = element.text
-            word_classification = element.tag
-            word_attributes = element.attrib
+            word = self._process_text(
+                text=element.text,
+                classification=element.tag,
+                attributes=element.attrib,
+                previous_word=words[-1] if words else None,
+            )
+            if word:
+                words.append(word)
 
-            if not word_text:
-                continue
-
-            db_word = crud_word.get_by_word(self.session, word=element.text)
-            if db_word:
-                if db_word.classification == word_classification:
-                    db_word = crud_word.link_to_epigraph(
-                        self.session,
-                        word=db_word,
-                        epigraph_id=self.epigraph.id,
+            for child in element:
+                if child.tail:
+                    word = self._process_text(
+                        child.tail,
+                        previous_word=words[-1] if words else None,
                     )
-                    previous_word = words[-1] if words else None
-                    if previous_word:
-                        db_word = crud_word.link_words(
-                            self.session,
-                            from_word=db_word,
-                            to_word=previous_word
-                        )
-                else:
-                    db_word = crud_word.create(
-                        db=self.session,
-                        obj_in=WordCreate(
-                            word=word_text,
-                            classification=word_classification,
-                            attributes=word_attributes,
-                        ),
-                    )
-            else:
-                db_word = crud_word.create(
-                    db=self.session,
-                    obj_in=WordCreate(
-                        word=word_text,
-                        classification=word_classification,
-                        attributes=word_attributes,
-                    ),
-                )
-                db_word = crud_word.link_to_epigraph(
-                    self.session,
-                    word=db_word,
-                    epigraph_id=self.epigraph.id
-                )
-                previous_word = words[-1] if words else None
-                if previous_word:
-                    db_word = crud_word.link_words(
-                        self.session,
-                        from_word=db_word,
-                        to_word=previous_word
-                    )
-
-            words.append(db_word)
+                    if word:
+                        words.append(word)
 
         return words
