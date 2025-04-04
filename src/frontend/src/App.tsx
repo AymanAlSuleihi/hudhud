a5b4e589062f0dac9a1d119761960ca5652bdb7b
Ayman <asuleihi@gmail.com>
2025-04-04 22:52:13 +0000

    Add refine

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,11 +1,14 @@
+import { Refine } from "@refinedev/core"
 import { Outlet } from "react-router-dom"
 import { Navbar } from "./components/Navbar"
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Outlet />
+      <Refine>
+        <Navbar />
+        <Outlet />
+      </Refine>
     </>
   )
 }
