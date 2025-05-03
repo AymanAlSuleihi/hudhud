2067ffe0bca9ad9e66be60891a0331a2110a2622
Ayman <asuleihi@gmail.com>
2025-05-03 19:33:30 +0000

    Add additional object fields

--- src/backend/app/app/models/object.py
+++ src/backend/app/app/models/object.py
@@ -12,6 +12,29 @@ from app.models.minimal import EpigraphMinimal, ObjectMinimal, SiteMinimal
 class ObjectBase(SQLModel):
     dasi_object: dict = Field(sa_column=Column(JSONB), default={})
     dasi_id: int
+    title: str
+    uri: str
+    start_date: Optional[str] = None
+    end_date: Optional[str] = None
+    period: Optional[str] = None
+    deposits: Optional[list[dict]] = Field(sa_column=Column(JSONB), default=[])
+    materials: Optional[list[str]] = Field(sa_column=Column(JSONB), default=[])
+    shape: Optional[str] = None
+    measures: Optional[str] = None
+    decorations: Optional[list[Union[dict, list]]] = Field(sa_column=Column(JSONB), default=[])
+    support_type_level_1: Optional[str] = None
+    support_type_level_2: Optional[str] = None
+    support_type_level_3: Optional[str] = None
+    support_type_level_4: Optional[str] = None
+    support_notes: Optional[str] = None
+    deposit_notes: Optional[str] = None
+    cultural_notes: Optional[list[dict]] = Field(sa_column=Column(JSONB), default=[])
+    bibliography: Optional[list[dict]] = Field(sa_column=Column(JSONB), default=[])
+    concordances: Optional[list[str]] = Field(sa_column=Column(JSONB), default=[])
+    license: str
+    editors: Optional[list[dict]] = Field(sa_column=Column(JSONB), default=[])
+    first_published: Optional[str] = None
+    last_modified: Optional[datetime] = None
 
 
 class ObjectCreate(ObjectBase):
@@ -21,6 +44,29 @@ class ObjectCreate(ObjectBase):
 class ObjectUpdate(SQLModel):
     dasi_object: Optional[dict] = Field(sa_column=Column(JSONB), default={})
     dasi_id: Optional[int] = None
+    title: Optional[str] = None
+    uri: Optional[str] = None
+    start_date: Optional[str] = None
+    end_date: Optional[str] = None
+    period: Optional[str] = None
+    deposits: Optional[list[dict]] = Field(sa_column=Column(JSONB), default=[])
+    materials: Optional[list[str]] = Field(sa_column=Column(JSONB), default=[])
+    shape: Optional[str] = None
+    measures: Optional[str] = None
+    decorations: Optional[list[Union[dict, list]]] = Field(sa_column=Column(JSONB), default=[])
+    support_type_level_1: Optional[str] = None
+    support_type_level_2: Optional[str] = None
+    support_type_level_3: Optional[str] = None
+    support_type_level_4: Optional[str] = None
+    support_notes: Optional[str] = None
+    deposit_notes: Optional[str] = None
+    cultural_notes: Optional[list[dict]] = Field(sa_column=Column(JSONB), default=[])
+    bibliography: Optional[list[dict]] = Field(sa_column=Column(JSONB), default=[])
+    concordances: Optional[list[str]] = Field(sa_column=Column(JSONB), default=[])
+    license: Optional[str] = None
+    editors: Optional[list[dict]] = Field(sa_column=Column(JSONB), default=[])
+    first_published: Optional[str] = None
+    last_modified: Optional[datetime] = None
 
 
 class Object(
