5e176f3f3a2bf7812279f6c5b7a45801a71e6396
Ayman <asuleihi@gmail.com>
2026-05-31 10:40:54 +0100

    Add Google Analytics and Umami tracking scripts to layout

--- src/frontend/app/layout.tsx
+++ src/frontend/app/layout.tsx
@@ -1,9 +1,13 @@
-import type { Metadata } from "next"
+import type { Metadata, Viewport } from "next"
 import { Montserrat, Noto_Sans_Old_South_Arabian } from "next/font/google"
+import Script from "next/script"
 import "./globals.css"
 import { Navbar } from "../src/components/Navbar"
 import { CookieConsent } from "../src/components/CookieConsent"
 import { defaultDescription, getSiteUrl, siteName } from "../src/next/lib/site"
+
+const googleAnalyticsId = "G-1BDTQ5V0QJ"
+const umamiWebsiteId = "04fe2800-b404-4930-a179-b56477a431d9"
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -26,6 +30,31 @@ export const metadata: Metadata = {
   description: defaultDescription,
   metadataBase: new URL(getSiteUrl()),
   applicationName: siteName,
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  keywords: [
+    "Ancient South Arabian",
+    "inscriptions",
+    "DASI",
+    "archaeology",
+    "epigraphy",
+    "pre-Islamic Arabia",
+    "Sabaic",
+    "Minaic",
+    "Qatabanic",
+    "Hadramitic",
+  ],
+  authors: [
+    {
+      name: "Sheba's Caravan",
+    },
+  ],
   icons: {
     icon: [
       {
@@ -36,6 +65,11 @@ export const metadata: Metadata = {
     ],
     shortcut: "/hudhud.svg",
   },
+}
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
 }
 
 interface RootLayoutProps {
@@ -49,6 +83,25 @@ export default function RootLayout({ children }: RootLayoutProps) {
       className={`${montserrat.variable} ${notoSansOldSouthArabian.variable}`}
     >
       <body>
+        <Script id="gtag-init" strategy="beforeInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+            gtag('js', new Date());
+            gtag('consent', 'default', { analytics_storage: 'denied' });
+            gtag('config', '${googleAnalyticsId}');
+          `}
+        </Script>
+        <Script
+          src={`/stats.js`}
+          data-website-id={umamiWebsiteId}
+          strategy="afterInteractive"
+        />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+          strategy="afterInteractive"
+        />
         <Navbar />
         {children}
         <CookieConsent />
