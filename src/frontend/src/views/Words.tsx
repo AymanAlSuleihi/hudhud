6d4fdc74c5c05049f8a0b90c4a9afabda4de089c
Ayman <asuleihi@gmail.com>
2026-06-01 19:23:41 +0100

    Add margin-top

--- src/frontend/src/views/Words.tsx
+++ src/frontend/src/views/Words.tsx
@@ -2,8 +2,8 @@ import React from "react"
 
 const Words: React.FC = () => {
   return (
-    <div className="flex  justify-center min-h-screen ">
-      <div className="text-center">
+    <div className="flex justify-center min-h-screen ">
+      <div className="text-center mt-20">
         <h1 className="text-2xl font-bold text-gray-700 mb-4">Coming Soon</h1>
         <p className="text-md text-gray-700 mb-8">
           The Words feature is currently under development.
